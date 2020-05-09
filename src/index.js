20e47914c6a607bb9fdb04bf82bcd1c85f9dd623
Maxim Roslow <brian2009@yandex.ru>
2020-05-10 00:00:50 +0400

    feat: add husky

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
+import store from './redux/store';
+
+import './index.css';
+import App from './App';
+
 ReactDOM.render(
   <React.StrictMode>
-    <Provider>
+    <Provider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
