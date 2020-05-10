30ccb2aef7a416d21d2f49dac5bfa2d1555b4b00
Maxim Roslow <brian2009@yandex.ru>
2020-05-10 13:07:07 +0400

    feat: menu item types

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,8 +1,18 @@
+//@flow
 import React from 'react';
 import { withRouter } from 'react-router-dom';
 import './menu-item.styles.scss';
 
-const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
+import type { TMenuItemProps } from './menu-item.types';
+
+const MenuItem = ({
+  title,
+  imageUrl,
+  size,
+  history,
+  linkUrl,
+  match,
+}: TMenuItemProps): React$Element<'div'> => (
   <div
     className={`${size} menu-item`}
     onClick={() => history.push(`${match.url}${linkUrl}`)}
---
+++ src/components/menu-item/menu-item.types.js
@@ -0,0 +1,9 @@
+//@flow
+export type TMenuItemProps = {
+  title: string,
+  imageUrl: string,
+  size: string,
+  history: string[],
+  linkUrl: string,
+  match: { url: string },
+};
