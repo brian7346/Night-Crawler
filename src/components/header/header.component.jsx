bdbf466e49cc81a9659efd08beec3b93359e1e56
Maxim Roslow <brian2009@yandex.ru>
2020-05-14 19:22:00 +0400

    feat: cart-icon component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
 import { auth } from '../../firebase/firebase.utils';
+import CartIcon from '../cart-icon/cart-icon.component';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
@@ -32,6 +33,7 @@ const Header = () => {
             SIGN IN
           </Link>
         )}
+        <CartIcon />
       </div>
     </div>
   );
