cc31161fa9beedcaee3c36e13cb263b9397733cc
Maxim Roslow <brian2009@yandex.ru>
2020-05-19 20:27:15 +0400

    feat: items quantity

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,6 +1,6 @@
 // @flow
 import React, { useCallback } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { toggleCartHidden } from '../../redux/cart/cart.actions';
 
 import type { TCartIconDispatch } from './cart-icon.types';
@@ -10,14 +10,19 @@ import ShoppingIcon from '../../assets/shopping-bag.svg';
 import './cart-icon.styles.scss';
 
 const CartIcon = (): React$Element<'div'> => {
+  const { cartItems } = useSelector(state => state.cart);
   const dispatch: TCartIconDispatch = useDispatch();
   const toggleHidden = useCallback(() => dispatch(toggleCartHidden()), [
     dispatch,
   ]);
+  const itemCount = cartItems.reduce(
+    (accumulatedQuantoty, cartItem) => accumulatedQuantoty + cartItem.quantity,
+    0
+  );
   return (
     <div className="cart-icon" onClick={toggleHidden}>
       <img src={ShoppingIcon} className="shopping-icon" alt="shopping" />
-      <span className="item-count">0</span>
+      <span className="item-count">{itemCount}</span>
     </div>
   );
 };
