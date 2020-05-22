3b66746a7abb7f94270d1a4dcc01701ba1797873
Maxim Roslow <brian2009@yandex.ru>
2020-05-22 22:11:22 +0400

    feat: checkout-item component

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
+import CheckoutItem from '../../components/checkout-item/checkout-item';
+
 import {
   selectCartItems,
   selectCartTotal,
@@ -36,7 +38,9 @@ const CheckoutPage = () => {
           <span>Remove</span>
         </div>
       </div>
-      {cartItems.map(cartItem => cartItem.name)}
+      {cartItems.map(cartItem => (
+        <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+      ))}
       <div className="total">
         <span>TOTAL: ${total}</span>
       </div>
