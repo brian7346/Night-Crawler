8bae3534aa89e7758489a065ba29bdfd72d57fe4
Maxim Roslow <brian2009@yandex.ru>
2020-05-22 18:48:05 +0400

    feat: cart-item types

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -1,8 +1,12 @@
+// @flow
 import React from 'react';
+
+import type { TCartItemProps } from './cart-item.types';
 
 import './cart-item.styles.scss';
 
-const CartItem = ({ item: { imageUrl, price, name, quantity } }) => {
+const CartItem = ({ item }: TCartItemProps): React$Element<'div'> => {
+  const { imageUrl, price, name, quantity } = item;
   return (
     <div className="cart-item">
       <img src={imageUrl} alt="item" />
---
+++ src/components/cart-item/cart-item.types.js
@@ -0,0 +1,6 @@
+// @flow
+import type { TCartItem } from '../../redux/cart/cart.types';
+
+export type TCartItemProps = {
+  item: TCartItem,
+};
